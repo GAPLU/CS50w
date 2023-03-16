13c0e42ccca11aa88b93f48026133d3c0fb5b2d8
GAPLU <igor.azovskiy@gmail.com>
2023-03-16 12:18:32 +0100

    rework stylings and html so it looks better

--- final_project/type_speed/static/type_speed/index.js
+++ final_project/type_speed/static/type_speed/index.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    load_text();
+    const button = document.getElementById("refresh-button");
+    button.addEventListener('click', () => load_text())
+
+
     const allowedChars = /^[A-Za-z0-9.,-]+$/;
 
     let currentLetterIndex = 1;
@@ -46,6 +51,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 
+    document.addEventListener('click', () => {
+        textInput.focus();
+    });
+ 
+});
+
+
+function load_text() {
+
+    const message = document.createElement('span');
+    message.classList.add('letter');
+    message.textContent = 'Text is loading...';
+
+    const textDiv = document.querySelector('#text-div');
+    textDiv.innerHTML = '';
+    textDiv.append(message);
+
+
+    const allowedChars = /^[A-Za-z0-9.,-]+$/;
 
     const options = {
         method: 'GET',
@@ -54,38 +78,36 @@ document.addEventListener('DOMContentLoaded', function() {
             'X-RapidAPI-Host': 'hargrimm-wikihow-v1.p.rapidapi.com'
         }
     };
-    
-    fetch('https://hargrimm-wikihow-v1.p.rapidapi.com/steps?count=100', options)
-        .then(response => response.json())
-        .then(texts => {
-            let letterCounter = 1;
-            for (const key in texts) {
-                if (texts.hasOwnProperty(key)) {
-                    const value = texts[key];
-                    for (let i = 0; i < value.length; i++) {
-                        const letter = value[i];
-                        if (letter.match(allowedChars) || letter === ' ') {
-                            const element = document.createElement('span');
-                            element.classList.add('letter');
-                            element.id = `letter${letterCounter}`;
-                            element.textContent = letter;
-                            document.querySelector('#text-div').append(element);
-                            
-                            if (letter == ' ') {
-                                const zeroWidthSpaceElement = document.createElement('span');
-                                zeroWidthSpaceElement.innerHTML = '&ZeroWidthSpace;';
-                                document.querySelector('#text-div').append(zeroWidthSpaceElement);
-                            }
 
-                            letterCounter++;
+    fetch('https://hargrimm-wikihow-v1.p.rapidapi.com/steps?count=100', options)
+    .then(response => response.json())
+    .then(texts => {
+        textDiv.innerHTML = '';
+        let letterCounter = 1;
+        for (const key in texts) {
+            if (texts.hasOwnProperty(key)) {
+                const value = texts[key];
+                for (let i = 0; i < value.length; i++) {
+                    const letter = value[i];
+                    if (letter.match(allowedChars) || letter === ' ') {
+                        const element = document.createElement('span');
+                        element.classList.add('letter');
+                        element.id = `letter${letterCounter}`;
+                        element.textContent = letter;
+                        textDiv.append(element);
+                        
+                        if (letter == ' ') {
+                            const zeroWidthSpaceElement = document.createElement('span');
+                            zeroWidthSpaceElement.innerHTML = '&ZeroWidthSpace;';
+                           textDiv.append(zeroWidthSpaceElement);
                         }
+
+                        letterCounter++;
                     }
                 }
             }
-            document.getElementById('letter1').classList.add('orange');
-        });
-    
-    document.addEventListener('click', () => {
-        textInput.focus();
+        }
+        document.getElementById('letter1').classList.add('orange');
     });
-});
+
+}
