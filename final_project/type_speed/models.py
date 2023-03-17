159e3ecca3fec1f0980f145933855afcca9ba252
GAPLU <igor.azovskiy@gmail.com>
2023-03-17 10:22:48 +0100

    add models for text and result

--- final_project/type_speed/models.py
+++ final_project/type_speed/models.py
@@ -3,3 +3,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Scores(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="scores")
+    words_min = models.IntegerField(default=0)
+    chars_min = models.IntegerField(default=0)
+    accuracy = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Done by: {self.user}"
+
+
+class CustomText(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="texts")
+    text = models.TextField()
+
+    def __str__(self):
+        return f"Written by: {self.user}"
